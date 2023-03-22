24f61765b3b09b7ff8ff8b1c0ccd6d3a0b644305
BagaevaMA <119247276+BagaevaMA@users.noreply.github.com>
2023-03-22 22:53:20 +0300

    Update Request.sql

--- Request.sql
+++ Request.sql
@@ -1,18 +1,18 @@
 select * from album
 where release_year = 2018
 
-select t.track_name as "Название трека", t.track_lengths as "Длительность трека" from track t
+select t.track_name as "РќР°Р·РІР°РЅРёРµ С‚СЂРµРєР°", t.track_lengths as "Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ С‚СЂРµРєР°" from track t
 order by t.track_lengths desc
 limit 1 
 
-select track_name as "Название трека", track_lengths as "Длительность трека"  from track t
+select track_name as "РќР°Р·РІР°РЅРёРµ С‚СЂРµРєР°", track_lengths as "Р”Р»РёС‚РµР»СЊРЅРѕСЃС‚СЊ С‚СЂРµРєР°"  from track t
 where t.track_lengths  >= '00:03:30'
 
-select collection_name as "Название сборника", collection_release_year as "Год выпуска сборника" from collection
+select collection_name as "РќР°Р·РІР°РЅРёРµ СЃР±РѕСЂРЅРёРєР°", collection_release_year as "Р“РѕРґ РІС‹РїСѓСЃРєР° СЃР±РѕСЂРЅРёРєР°" from collection
 where collection_release_year between 2018 and 2020
 
 select * from artist
 where artist_name not like '% %'
 
 select * from track
-where lower(track_name) like '%мой%' or lower(track_name) like '%my%'
+where lower(track_name) like '%РјРѕР№%' or lower(track_name) like '%my%'
